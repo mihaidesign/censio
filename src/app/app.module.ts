7abc0f1580c75e58d9be8a5470ac8811c495da8d
Mihai Florea <mihaifloreadesign@gmail.com>
2018-06-11 15:28:55 +0300

    added users component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ReviewsComponent } from './reviews/reviews.component';
+import { UsersComponent } from './users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ReviewsComponent
+    ReviewsComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
