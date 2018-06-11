e90c4a46ea96506a356308f3ebce0186eb0bb0a7
Mihai Florea <mihaifloreadesign@gmail.com>
2018-06-11 16:46:28 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersComponent
+  },
+  {
+    path: 'details/:id',
+    component: UserDetailsComponent
+  },
+  {
+    path: 'reviews',
+    component: ReviewsComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
